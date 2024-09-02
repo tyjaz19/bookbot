6a0f53227d24cd28ae0acc17c626930151c5e121
tyjaz19 <tyjazynka@proton.me>
2024-09-01 21:09:22 -0400

    add word count fun

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 def main ():
-    with open("books/frankenstein.txt") as f:
-        file_contents=f.read()
-    print(file_contents)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(word_count(text))
 
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def word_count(text):
+    indvi_words = text.split()
+    return len(indvi_words)
+    
 main()
